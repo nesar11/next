97980c5cd511fe460d87600632aff79a4dfbf251
nesar11 <nesar.uddin100@gmail.com>
2022-06-20 21:49:09 +0800

    first commit

---
+++ pages/api/contact.js
@@ -0,0 +1,32 @@
+export default function (req, res) {
+  require('dotenv').config()
+
+  let nodemailer = require('nodemailer')
+  const transporter = nodemailer.createTransport({
+    port: 465,     
+    host: "smtp.gmail.com",
+       auth: {
+            user: 'wevpaytesting@gmail.com',
+            pass: process.env.password,
+         },
+    secure: true,
+  });
+  
+  const mailData = {
+      from: 'wevpaytesting@gmail.com',
+      to: `${req.body.name}`,
+      subject: `Message From ${req.body.name}`,
+      text: req.body.message + " | Sent from: " + req.body.email,
+      html: `<div>${req.body.message}</div><p>Sent from: ${req.body.email}</p>`
+  }
+
+  transporter.sendMail(mailData, function (err, info) {
+      if(err)
+        console.log(err)
+      else
+        console.log(info);
+  })
+
+  console.log(req.body)
+  res.send('success')
+}
